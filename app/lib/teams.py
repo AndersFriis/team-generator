0e50935ebd1bbb97db5efcc2517dd8b0d53945da
tmcothran <tmcothran@hotmail.com>
2016-09-12 15:57:39 -0400

    User can generate new teams. Still some bugs--doesn't deal with uneven teams, for instance.

--- app/lib/teams.py
+++ app/lib/teams.py
@@ -1,23 +1,49 @@
-from random import shuffle
+from random import shuffle, choice
+import json
+
 
 class Team():
-    def __init__(self, players):
+    def __init__(self, players, team_name):
         self.players = players
-        self.name = self.get_team_name()
-
-    def get_team_name(self):
-        """Returns a single string with an adjective and a noun."""
-        return "A team name"
-
+        self.name = team_name
 
 class GroupOfTeams():
     def __init__(self):
         self.teams = []
-        self.get_teams()
+        self.adjectives = []
+        self.nouns = []
+
+    def setup(self):
+        self.set_adjectives()
+        self.set_nouns()
+
+    def set_adjectives(self):
+        with open('assets/adjectives.txt') as inputAdjectives:
+            self.adjectives = inputAdjectives
+            # Strip Off Newlines
+            self.adjectives = [line.rstrip('\n').split(',') for line in self.adjectives]
+            # Flatten List
+        self.adjectives = [item for sublist in self.adjectives for item in sublist]
+
+    def set_nouns(self):
+        with open('assets/nouns.txt') as inputNouns:
+            self.nouns = inputNouns
+            """ Strip Off Newlines"""
+            self.nouns = [line.rstrip('\n').split(',') for line in self.nouns]
+            """ Flatten List """
+        self.nouns = [item for sublist in self.nouns for item in sublist]
+
+    def get_team_name(self):
+        """Returns a single string with an adjective and a noun."""
+        noun = choice(self.nouns)
+        adjective = choice(self.adjectives)
+        return "{} {}".format(noun.capitalize(), adjective.capitalize())
 
     def get_players(self):
         """Returns a list of all players."""
-        return ["A list", "of players"]
+        with open('assets/students.json', 'r') as f:
+            list_of_students = json.load(f)
+        return list_of_students
 
     def get_teams(self):
         # List of tuples for player pairings
@@ -28,4 +54,9 @@ class GroupOfTeams():
         while full_list_of_players:
             first_player = full_list_of_players.pop()
             second_player = full_list_of_players.pop()
-            self.teams.append(Team([first_player, second_player]))
+            team_name = self.get_team_name()
+            new_team = Team(
+                [first_player, second_player],
+                team_name
+            )
+            self.teams.append(new_team)
