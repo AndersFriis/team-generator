f87a76b7566df0933bd4c89938d1724c9be302eb
tmcothran <tmcothran@hotmail.com>
2016-09-12 14:37:07 -0400

    Teams module is accessable in views and templates.

---
+++ app/lib/teams.py
@@ -0,0 +1,6 @@
+class Team():
+    pass
+
+class GroupOfTeams():
+    def __init__(self):
+        self.text = "Hello World!"
--- app/views.py
+++ app/views.py
@@ -1,14 +1,11 @@
 from flask import render_template
 
 from app import app
+from app.lib.teams import GroupOfTeams
 
 @app.route("/")
 @app.route("/index")
 def index():
-    user = {
-        "full_name": "Matt Smith",
-        "username": "mattsmith1",
-    }
+    group_of_teams = GroupOfTeams()
 
-
-    return render_template("index.html", user=user)
+    return render_template("index.html", group_of_teams=group_of_teams)
